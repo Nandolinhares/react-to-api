b398161f698f9c2387fb7a04f3686aaffd633c4c
Nando Linhares <fernandolinhares1920@globo.com>
2019-03-28 21:59:42 -0300

    Added arrow function loadProj and showing products on the window

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react';
-
 import api from '../../services/api';
+//import { Link } from 'react-router-dom';
 
 export default class Main extends Component{
     
+    state = {
+        products: []
+    }
     
+    componentDidMount(){
+        this.loadProducts();
+    }
+
+    loadProducts = async () =>{
+        const response = await api.get(`/api/products`);
+        
+        this.setState({ products: response.data.docs });
+
+        console.log(response.data);
+    }
         
     render(){
+        const { products } = this.state;
         return(
-            <h1>Main content</h1>
+            <div className="product-list">
+                {products.map(product => (
+                    <article key={product._id}>
+                        <strong>{product.title}</strong>
+                        <p>{product.description}</p>
+                        
+                        
+                    </article>
+                ))}
+            </div>
         );
     }
 }
