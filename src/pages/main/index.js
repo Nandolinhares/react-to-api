758c47c681882c79c1f336f1064752081b30a681
Nando Linhares <fernandolinhares1920@globo.com>
2019-03-28 22:31:52 -0300

    Finishing button dinamics. Arrow functions nextPage and prevPage done

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,24 +2,48 @@ import React, { Component } from 'react';
 import api from '../../services/api';
 //import { Link } from 'react-router-dom';
 
+import './styles.css'
+
 export default class Main extends Component{
     
     state = {
-        products: []
+        products: [],
+        productInfo: {},
+        page: 1
     }
     
     componentDidMount(){
         this.loadProducts();
     }
 
-    loadProducts = async () =>{
-        const response = await api.get(`/api/products`);
-        
-        this.setState({ products: response.data.docs });
+    loadProducts = async (page = 1) =>{
+        const response = await api.get(`/api/products?page=${page}`);
 
-        console.log(response.data);
+        const { docs, ...productInfo } = response.data
+        
+        this.setState({ products: docs, productInfo, page });
+
+        console.log(productInfo);
+    }
+
+    prevPage = () =>{
+        const { page } = this.state;
+
+        if(page === 1) return;
+
+        const pageNumber = page - 1;
+        this.loadProducts(pageNumber);
     }
         
+    nextPage = () =>{
+        const { page, productInfo } = this.state;
+
+        if( page === productInfo.pages ) return;
+
+        const pageNumber = page + 1;
+        this.loadProducts(pageNumber);
+    }
+
     render(){
         const { products } = this.state;
         return(
@@ -32,6 +56,10 @@ export default class Main extends Component{
                         
                     </article>
                 ))}
+                <div className="actions">
+                    <button onClick={this.prevPage}>Anterior</button>
+                    <button onClick={this.nextPage}>Próxima</button>
+                </div>
             </div>
         );
     }
