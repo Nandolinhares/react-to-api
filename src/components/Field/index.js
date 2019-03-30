94d938999fef943ed3d2892131b6e635275f3ac1
Nando Linhares <fernandolinhares1920@globo.com>
2019-03-29 22:42:58 -0300

    Form to add to api database done sucessfull

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -5,7 +5,7 @@ import api from '../../services/api';
 //import axios from 'axios';
 
 export default class Field extends Component{
-    
+
      componentDidMount(){
         //const response = await api.get(`/api/products`);
        /*const response = api.post('/api/products', { 
@@ -15,18 +15,47 @@ export default class Field extends Component{
          });
 
         console.log(response);*/
+        const formEl = document.getElementById('body-form');
+        const titleEl = document.querySelector('input[name=title]');
+        const descriptionEl = document.querySelector('input[name=description]');
+
+        this.state = {
+            formEl: formEl,
+            titleEl: titleEl,
+            descriptionEl
+        }
     }
     
+    /*registerHandlers = () =>{
+        const { formEl } = this.state;
+        formEl.onsubmit = this.insertData();
+    }*/
+
+    insertData = async () =>{
+        //event.preventDefault();
+        const { titleEl, descriptionEl } = this.state;
+
+        const inputTitle = titleEl.value;
+        const inputDescription = descriptionEl.value;
+
+           const response = await api.post('/api/products', {
+               title: inputTitle,
+               description: inputDescription,
+               url:'io'
+            });
+          
+    }
 
     render(){
         return(
-            <form id="body-form">
+            <form id="body-form" action="" onSubmit={this.insertData}>
                 <label>Title: </label>
                 <input type="text" name="title" id="title"></input>
                 <label>Description: </label>
                 <input type="text" name="description" id="description"></input>
-                <button type="submit"></button>
+                <button type="submit">Adicionar</button>
             </form>
         );
     }
 }
+/*onClick={this.insertData}*/
